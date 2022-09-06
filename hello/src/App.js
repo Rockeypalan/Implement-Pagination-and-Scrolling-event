958e2f0369596d9ac9ba1d3946da06a567b7c25c
RAKESH R <94379161+Rockeypalan@users.noreply.github.com>
2022-09-06 11:22:10 +0530

    first

---
+++ hello/src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import Pagination from "./Pagination";
+import InfiniteScroll from "./InfiniteScroll";
+import { useState } from "react";
+
+function App() {
+  const [view, setView] = useState("pagination");
+
+  // const Pagination = () => {};
+  // {
+  //   <Pagination />;
+  // }
+  // const InfiniteScroll = () => {};
+  // {
+  //   <InfiniteScroll />;
+  // }
+  return (
+    <div>
+      <h1>Welcome to Random Users</h1>
+
+      <div className="nav">
+        <button onClick={() => setView("pagination")}>Pagination</button>
+        <button onClick={() => setView("infiniteScroll")}>
+          Infinite Scroll
+        </button>
+      </div>
+      {view === "pagination" ? <Pagination /> : <InfiniteScroll />}
+    </div>
+  );
+}
+
+export default App;
---
+++ hello/src/InfiniteScroll.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+
+function InfiniteScroll() {
+  const [data, setData] = useState([]);
+  const [offset, setOffset] = useState(0);
+
+  const loadUser = () => {
+    axios.get(`https://dummyjson.com/products?offset=${offset}`).then((res) => {
+      console.log(res.data.products);
+      const data = res.data.products;
+      setData((pre) => [...pre, ...data]);
+    }, []);
+  };
+
+  useEffect(() => {
+    loadUser();
+  }, [offset]);
+
+  useEffect(() => {
+    const handleScroll = (e) => {
+      const scrollHeight = e.target.documentElement.scrollHeight;
+      const currentHeight =
+        e.target.documentElement.scrollTop + window.innerHeight;
+      if (currentHeight + 1 >= scrollHeight) {
+        setOffset(offset + 5);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [offset]);
+
+  return (
+    <div>
+      <h2>Infinite Scroll View</h2>
+      <div className="card">
+        {data &&
+          data.map((product) => (
+            <div className="product-item" key={product.id}>
+              <img
+                src={product.thumbnail}
+                alt={product.title}
+                className="img"
+              />
+              <div className="product-info">
+                <h4>{product.title}</h4>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default InfiniteScroll;
